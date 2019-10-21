f71cdbe6462f55ef64850bada890e4d4d213196d
igordefo <dekleidung@gmail.com>
2019-10-21 12:09:57 +0300

    feat: implement lazy load images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,4 @@
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map('map', {
-    center: [47.24471976300841, 39.72322170928197],
-    zoom: 16,
-    controls: ['zoomControl']
-  });
-
-  myPlacemark = new ymaps.Placemark(
-    [47.24471976300841, 39.72322170928197],
-    {
-      hintContent: 'Shopping Center Decorum',
-      balloonContent: 'Rostov-on-Don, Nansen St., 239'
-    },
-    {
-      iconLayout: 'default#image',
-      iconImageHref: 'assets/img/pin.svg',
-      iconImageSize: [30, 45],
-      iconImageOffset: [-18, -50]
-    }
-  );
-
-  myMap.behaviors.disable(['scrollZoom', 'drag']);
-  myMap.geoObjects.add(myPlacemark);
-}
+// Smooth scroll
 
 const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1200,
@@ -159,3 +135,57 @@ $(document).ready(function() {
 $(window).on('resize orientationchange', function() {
   $('.gallery').slick('resize');
 });
+
+// Images lazy loading
+
+const images = document.querySelectorAll('[data-lazy]');
+
+const lazyLoad = target => {
+  const io = new IntersectionObserver((entries, observer) => {
+    console.log(entries);
+    entries.forEach(entry => {
+      console.log('img');
+
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-lazy');
+
+        img.setAttribute('srcset', src);
+
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+images.forEach(lazyLoad);
+
+// Yandex map
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map('map', {
+    center: [47.24471976300841, 39.72322170928197],
+    zoom: 16,
+    controls: ['zoomControl']
+  });
+
+  myPlacemark = new ymaps.Placemark(
+    [47.24471976300841, 39.72322170928197],
+    {
+      hintContent: 'Shopping Center Decorum',
+      balloonContent: 'Rostov-on-Don, Nansen St., 239'
+    },
+    {
+      iconLayout: 'default#image',
+      iconImageHref: 'assets/img/pin.svg',
+      iconImageSize: [30, 45],
+      iconImageOffset: [-18, -50]
+    }
+  );
+
+  myMap.behaviors.disable(['scrollZoom', 'drag']);
+  myMap.geoObjects.add(myPlacemark);
+}
